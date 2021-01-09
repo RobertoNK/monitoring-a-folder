9cdce78253572ea6ace0eb75638bbc965eca17ca
unknown <you@example.com>
2021-01-08 18:13:01 -0800

    Printing text file name when it is created

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import sys
+import os
 import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import glob
 
 def on_created(event):
+    os.chdir("D:/server/www/test_folder")
+    list_of_files = glob.glob("*.txt")
+    list_of_files.sort(key=os.path.getmtime)
+    last_file = list_of_files[len(list_of_files)-1]
+    print(last_file)
+    #print(last_file)
     print("created")
         
 def on_deleted(event):
@@ -16,26 +24,26 @@ def on_modified(event):
 def on_moved(event):
     print("moved")
 
-if __name__ == "__main__":
-   
-    event_handler = FileSystemEventHandler()
 
+if __name__ == "__main__":
+    event_handler = FileSystemEventHandler()
     # calling functions
     event_handler.on_created = on_created
     event_handler.on_deleted = on_deleted
     event_handler.on_modified = on_modified
     event_handler.on_moved = on_moved
 
-    path="D:/server/www/test_folder"
+
+    path = "D:/server/www/test_folder"
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
-
     observer.start()
     try:
-        print("Monitoring")
+        print("Monitoreando")
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
-        print("Done")
+        print("terminado")
     observer.join()
+    
